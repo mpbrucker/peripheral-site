a8bbde033ed40b9c47422921b51fc632f51cfd00
Matt Brucker <mpbrucker@gmail.com>
2025-05-29 13:22:18 -0500

    modifying styling of subscribe form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,19 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image';
-import Button from '@mui/material/Button';
+import logo from "../images/peripheral-study.svg";
+import { Form, TextField } from "radix-ui";
 
 const IndexPage = ({ data }) => {
   return (
     <Layout pageTitle="Home Page">
-      {/* <StaticImage
-        className = "logo-img"
-        loading="eager"
-        placeholder="none"
-        src="../images/ps-logo.png"
-      /> */}
-      <h2 className = "page-title">Notes on Peripheral Study</h2>
-      <div className = "page-subtitle-wrapper">
-        Writing for more critical living
-      </div>
-      <div className = "subscribe-container">
-      {/* <div id="mc_embed_shell">
-  
-        <div id="mc_embed_signup">
-            <form action="https://gmail.us13.list-manage.com/subscribe/post?u=abfd043ab1f43a59c3d6e2b53&amp;id=763f4bc72a&amp;f_id=00c04ce1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
-                <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
-                    <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-                    <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div>
-                <div id="mce-responses" class="clear">
-                    <div class="response" id="mce-error-response" style="display: none;"></div>
-                    <div class="response" id="mce-success-response" style="display: none;"></div>
-                </div><div aria-hidden="true" style="position: absolute; left: -5000px;"><input type="text" name="b_abfd043ab1f43a59c3d6e2b53_763f4bc72a" tabindex="-1" value=""></div><div class="clear"><input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe"></div>
-            </div>
-        </form>
-        </div>
-        </div> */}
-  
-        <Button variant="contained">Subscribe</Button>
-      </div>
+      <img src = {logo} alt="SVG text reading Notes on Peripheral Study."/>
+      <span className = "page-subtitle-wrapper">
+        thinking and practice for more critical living
+      </span>
+      <h2>
+        *****
+      </h2>
       <div className="post-list">
         <ol>
           {
@@ -43,6 +21,7 @@ const IndexPage = ({ data }) => {
                 <li key={node.id} className="post-list-item">
                   <div className = "post-list-item-header">
                     <h3>{node.frontmatter.title}</h3>
+                    <h5>{node.frontmatter.date}</h5>
                   </div>
                   <div className = "post-list-item-content">
                     <p>{node.frontmatter.description}</p>
@@ -51,6 +30,31 @@ const IndexPage = ({ data }) => {
             ))
           }
         </ol>
+      </div>
+      <h2>
+        *****
+      </h2>
+
+      <div className = "subscribe-container">
+        <Form.Root className="subscribe-form-root">
+          <Form.Field name="email">
+              <Form.Message className="subscribe-form-message" match="valueMissing">
+                Please enter your email
+              </Form.Message>
+              <Form.Message className="subscribe-form-message" match="typeMismatch">
+                Please provide a valid email
+              </Form.Message>
+            <Form.Control asChild>
+              <input className="subscribe-form-input" type="email" placeholder="youremail@example.com" required />
+            </Form.Control>
+          </Form.Field>
+
+          <Form.Submit asChild>
+            <button className="subscribe-submit-button" >
+              Subscribe
+            </button>
+          </Form.Submit>
+        </Form.Root>
       </div>
     </Layout>
   )
